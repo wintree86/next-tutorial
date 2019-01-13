33c8ccef7185cccd706d9653ade5a5583768769b
RyanCho <mith1004@gmail.com>
2019-01-13 10:51:03 +0900

    add scss

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,15 @@
-export default () => <div>Welcome to next.js</div>
+import React from "react";
+import "../styles.scss";
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="example">
+        Hello world 
+        <p>scoped!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
